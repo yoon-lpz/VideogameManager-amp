c5955ced70b089b5b48178ecd8beda1d06504366
Yoon <yoon.lopez.7e9@itb.cat>
2026-05-10 23:56:28 +0200

    Added developer to Index, refs #5

--- Pages/Games/Index.cshtml.cs
+++ Pages/Games/Index.cshtml.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using VideoGameManager.Data;
 using VideoGameManager.Models;
 using VideoGameManager.Services;
 
@@ -7,17 +8,53 @@ namespace VideoGameManager.Pages.Games
 {
     public class IndexModel : PageModel
     {
-        private readonly GameService _gameService;
+        private readonly GameStoreContext _context;
         public List<Game> Games { get; set; } = new();
 
-        public IndexModel(GameService gameService)
+        public IndexModel(GameStoreContext context)
         {
-            _gameService = gameService;
+            _context = context;
         }
 
         public void OnGet()
         {
-            Games = _gameService.GetAll();
+            Games = _context.Games.ToList();
+            if (!_context.Developers.Any())
+            {
+                var dev1 = new Developer { Name = "Nintendo", Country = "Japan", FoundedYear = 1889 };
+                var dev2 = new Developer { Name = "CD Projekt Red", Country = "Poland", FoundedYear = 1994 };
+                _context.Developers.AddRange(dev1, dev2);
+                _context.SaveChanges();
+
+                _context.Games.AddRange(
+                    new Game
+                    {
+                        Title = "The Legend of Zelda: TotK",
+                        Genre = "Adventure",
+                        Year = 2023,
+                        Score = 9.8,
+                        DeveloperId = dev1.Id
+                    },
+                    new Game
+                    {
+                        Title = "Mario Kart 8",
+                        Genre = "Racing",
+                        Year = 2014,
+                        Score = 8.7,
+                        DeveloperId = dev1.Id
+                    },
+                    new Game
+                    {
+                        Title = "The Witcher 3",
+                        Genre = "RPG",
+                        Year = 2015,
+                        Score = 9.5,
+                        DeveloperId = dev2.Id
+                    }
+                );
+                _context.SaveChanges();
+            }
+
         }
     }
 }
