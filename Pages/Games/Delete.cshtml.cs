ffc0ca5b2b93db62b037d83861637bbf4af223d0
Yoon <yoon.lopez.7e9@itb.cat>
2026-05-11 00:57:23 +0200

    Delete screen needs fix, refs #3

--- Pages/Games/Delete.cshtml.cs
+++ Pages/Games/Delete.cshtml.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using VideoGameManager.Data;
 using VideoGameManager.Models;
 using VideoGameManager.Services;
 
@@ -7,27 +9,49 @@ namespace VideoGameManager.Pages.Games
 {
     public class DeleteModel : PageModel
     {
-        private readonly GameService _gameService;
+        private readonly GameStoreContext _context;
 
         public int EmptyStars, FilledStars;
 
         [BindProperty]
         public Game Game { get; set; }
 
-        public DeleteModel(GameService gameService)
+        public DeleteModel(GameStoreContext context) => _context = context;
+
+        public async Task<IActionResult> OnGetAsync(int id)
         {
-            _gameService = gameService;
+            Game = await _context.Games
+                .Include(g => g.Developer)
+                .FirstOrDefaultAsync(m => m.Id == id);
+
+            if (Game == null)
+            {
+                return NotFound();
+            }
+
+            FilledStars = (int)(Game.Score / 2);
+            EmptyStars = 5 - FilledStars;
+
+            return Page();
         }
 
-        public void OnGet(int id)
+        public async Task<IActionResult> OnPostAsync()
         {
-            Game = _gameService.GetById(id);
-            FilledStars = (int)Game.Score / 2;
-            EmptyStars = 5 - FilledStars;
-        }
-        public IActionResult OnPost()
-        {
-            _gameService.Delete(Game.Id);
+            var gameToDelete = await _context.Games.FindAsync(Game.Id);
+
+            if (gameToDelete != null)
+            {
+                try
+                {
+                    _context.Games.Remove(gameToDelete);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateException)
+                {
+                    return RedirectToPage("./Delete", new { id = Game.Id, saveChangesError = true });
+                }
+            }
+
             return RedirectToPage("./Index");
         }
     }
