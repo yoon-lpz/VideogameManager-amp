b40250436ef8ca4217ca8321c08c3b8d471de1ff
Yoon <yoon.lopez.7e9@itb.cat>
2026-05-11 00:47:29 +0200

    Modified details page, refs #5

--- Pages/Games/Details.cshtml.cs
+++ Pages/Games/Details.cshtml.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using VideoGameManager.Data;
 using VideoGameManager.Models;
 using VideoGameManager.Services;
 
@@ -8,19 +10,27 @@ namespace VideoGameManager.Pages.Games
     public class DetailsModel : PageModel
     {
         public int EmptyStars, FilledStars;
-        private readonly GameService _gameService;
+        private readonly GameStoreContext _context;
         public Game Game { get; set; }
 
-        public DetailsModel(GameService gameService)
-        {
-            _gameService = gameService;
-        }
 
-        public void OnGet(int id)
+        public DetailsModel(GameStoreContext context) => _context = context;
+
+        public async Task<IActionResult> OnGetAsync(int id)
         {
-            Game = _gameService.GetById(id);
-            FilledStars = (int) Game.Score / 2;
+            Game = await _context.Games
+                .Include(g => g.Developer)
+                .FirstOrDefaultAsync(m => m.Id == id);
+
+            if (Game == null)
+            {
+                return NotFound();
+            }
+
+            FilledStars = (int)(Game.Score / 2);
             EmptyStars = 5 - FilledStars;
+
+            return Page();
         }
     }
 }
